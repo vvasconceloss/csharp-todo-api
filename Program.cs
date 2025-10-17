ebdb4621631d928d81bce4187cec52767f2df572
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-17 15:52:44 +0100

    feat(project): register the user create DTO validator in program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
+using FluentValidation;
+using csharp_todo_api.DTOs;
 using csharp_todo_api.Context;
 using csharp_todo_api.Middlewares;
+using csharp_todo_api.Validations;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +10,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<IValidator<UserCreateDTO>, UserCreateDTOValidator>();
 
 builder.Services.AddOpenApi();
 
