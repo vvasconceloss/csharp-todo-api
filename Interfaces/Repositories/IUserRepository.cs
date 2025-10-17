4c58c6c25bba0c3311706cb2bfcbb2349f8d25bb
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-17 13:05:21 +0100

    chore(interface): remove the user repository interface (unused)
    - the user interface methods coincide with those of authentication

--- Interfaces/Repositories/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-using csharp_todo_api.Models;
-
-namespace csharp_todo_api.Interfaces.Repositories;
-
-public interface IUserRepository
-{
-    Task<User> CreateAsync(User user);
-    Task<User?> GetByEmailAsync(string email);
-}
