a68aa1b0493f9e220c631472ab5e8d106246386a
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-17 14:06:21 +0100

    feat(repositories): inject db context dependency

--- Repositories/AuthRepository.cs
+++ Repositories/AuthRepository.cs
@@ -1,10 +1,18 @@
 using csharp_todo_api.Models;
+using csharp_todo_api.Context;
 using csharp_todo_api.Interfaces.Repositories;
 
 namespace csharp_todo_api.Repositories;
 
 public class AuthRepository : IAuthRepository
 {
+    private readonly AppDbContext _context;
+
+    public AuthRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
     public Task<User> RegisterAsync(User user, string password)
     {
         throw new NotImplementedException();
