715173d97c3046805512be28515fb1cfc6273db6
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-17 14:49:13 +0100

    feat(services): inject the auth repository dependency into the auth service

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,10 +1,18 @@
 using csharp_todo_api.DTOs;
+using csharp_todo_api.Repositories;
 using csharp_todo_api.Interfaces.Services;
 
 namespace csharp_todo_api.Services;
 
 public class AuthService : IAuthService
 {
+    private readonly AuthRepository _repo;
+
+    public AuthService(AuthRepository repository)
+    {
+        _repo = repository;
+    }
+
     public Task<UserResponseDTO> RegisterAsync(UserCreateDTO user)
     {
         throw new NotImplementedException();
