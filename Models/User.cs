ca50be793c0c1dfbec0d4b3faa4f25b283c9ec5a
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-17 12:34:37 +0100

    feat(models): create a model that represents the user entity
    - define the main fields of the entity

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+namespace csharp_todo_api.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string passwordHash { get; set; } = string.Empty;
+    public string? Username { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
