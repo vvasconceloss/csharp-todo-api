5f8b7378e48abb62920e2071855a3fc9ef7b14e8
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-17 15:16:28 +0100

    chore(exceptions): change the status code of the base exception to status 500

--- Exceptions/BaseException.cs
+++ Exceptions/BaseException.cs
@@ -4,7 +4,7 @@ public class BaseException : Exception
 {
     public int StatusCode { get; }
 
-    public BaseException(string message, int statusCode = StatusCodes.Status400BadRequest) : base(message)
+    public BaseException(string message, int statusCode = StatusCodes.Status500InternalServerError) : base(message)
     {
         StatusCode = statusCode;
     }
