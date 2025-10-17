a0eeb97cd016e5f39585eaf8c08e3c3b9b433b6d
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-17 12:52:06 +0100

    feat(context): configure the db context in program.cs to use the sqlite connector

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
+using csharp_todo_api.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 builder.Services.AddOpenApi();
 
