927f9c1bedc8d14c808347e17dc8ca434c1a7df2
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-20 21:24:01 +0100

    feat(controllers): use validations within the auth controller itself (registration route)

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using csharp_todo_api.DTOs;
 using csharp_todo_api.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -8,16 +9,26 @@ namespace csharp_todo_api.Controllers;
 public class AuthController : ControllerBase
 {
     private readonly AuthService _service;
+    private readonly IValidator<UserCreateDTO> _validator;
 
-    public AuthController(AuthService service)
+    public AuthController(AuthService service, IValidator<UserCreateDTO> validator)
     {
         _service = service;
+        _validator = validator;
     }
 
     [HttpPost]
     [Route("user/register")]
     public async Task<ActionResult<UserResponseDTO>> Register(UserCreateDTO userDTO)
     {
+        var validationResult = await _validator.ValidateAsync(userDTO);
+
+        if (!validationResult.IsValid)
+        {
+            var errors = validationResult.Errors.Select(error => new { error.PropertyName, error.ErrorMessage });
+            return BadRequest(errors);
+        }
+
         return Ok(await _service.RegisterAsync(userDTO));
     }
 
